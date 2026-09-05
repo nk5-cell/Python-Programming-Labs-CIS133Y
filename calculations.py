4bc83100c00ebe89edb723430f9bdcfee6a5851d
Nidhi Kairon <253822326+nk5-cell@users.noreply.github.com>
2026-09-05 00:22:16 -0700

    Add deposit and balance functions in calculations.py

---
+++ calculations.py
@@ -0,0 +1,7 @@
+def deposit(currentBalance, amount):
+    currentBalance1 = amount + currentBalance
+    return currentBalance1
+
+def balance(currentBalance, amount):
+    currentBalance2 =  amount - currentBalance
+    return currentBalance2
